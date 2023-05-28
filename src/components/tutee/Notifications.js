41e9d6da0bbbb1fc23ffa51a45f4b950e8392ec2
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-05-28 11:17:34 +0530

    Notifications.css added

--- src/components/tutee/Notifications.js
+++ src/components/tutee/Notifications.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from 'react';
+import NavBar from './NavBar';
+import './Notifications.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function Notifications() {
   return (
     <>
-    <NavBar/>
-    <div>No Notifications yet</div>
+      <NavBar />
+      <div className="notifications-container">
+        <div className="notifications-header">
+          <h2 className="notifications-heading">Notifications</h2>
+        </div>
+        <div className="notifications-content">
+          <div className="notification">
+            <div className="notification-icon">
+              <FontAwesomeIcon icon={faBell} />
+            </div>
+            <div className="notification-text">New event starting soon!</div>
+          </div>
+          <div className="notification">
+            <div className="notification-icon">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </div>
+            <div className="notification-text">You have a new message</div>
+          </div>
+          <div className="notification">
+            <div className="notification-icon">
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+            <div className="notification-text">New friend request</div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
