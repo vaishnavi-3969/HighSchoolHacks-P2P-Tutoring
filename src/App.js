6ae2b94470b850bfe37b9f0de37f30cd8bff44ce
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-03 18:33:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar';
-import Home from './components/Home';
-import Font, { Text } from 'react-font'
-import Tutors from './components/tutor/Tutors';
-import Tutees from './components/tutee/Tutees';
-import Profile from './components/Profile';
-import PreviousSessions from './components/tutor/PreviousSessions';
-import CreateSession from './components/tutor/CreateSession';
-import TutorNotifications from './components/tutor/Notifications';
-import TuteeNotification from './components/tutee/Notifications';
-import TutorMessages from './components/tutor/Messages';
-import TuteeMessages from './components/tutee/Messages';
-import AllSession from './components/tutee/AllSession';
-import UpcomingSessions from './components/tutee/UpcomingSessions';
-import TutorsPage from './components/TutorsPage';
+import Home from './Home';
 
 function App() {
   return (
@@ -25,18 +12,6 @@ function App() {
         <div id="page-body">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/tutors" element={<Tutors />} />
-            <Route path="/tutees" element={<Tutees />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/previous_sessions" element={< PreviousSessions />} />
-            <Route path="/create_session" element={< CreateSession />} />
-            <Route path="/tutor_notifications" element={< TutorNotifications />} />
-            <Route path="/tutor_messages" element={< TutorMessages />} />
-            <Route path="/all_sessions" element={<AllSession />} />
-            <Route path="/upcoming_sessions" element={< UpcomingSessions />} />
-            <Route path="/tutee_notifications" element={<TuteeNotification />} />
-            <Route path="/tutee_messages" element={<TuteeMessages />} />
-            <Route path='/tutors_page' element = {<TutorsPage/>}/>
           </Routes>
         </div>
       </div>
